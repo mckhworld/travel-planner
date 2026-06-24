6bbffbae4b283e9421801cfae515a77549814b8a
Mic <cheung@MicdeMacBook-Pro.local>
2026-06-24 13:22:48 +0800

    deploy: rebuild Vue app (2026-06-24)

--- assets/utils/maps-link.js
+++
@@ -1,5 +0,0 @@
-export function buildMapsLink(place) {
-    const parts = [place.name, place.area, place.address].filter(Boolean)
-    const query = parts.join(' ')
-    return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}&hl=zh-TW`
-}
