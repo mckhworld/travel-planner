22e8f3f234b4bef2fb057c7c84d3a8cba02d8441
Mic <mic@MicMacBookPro>
2026-07-01 23:30:31 +0800

    fix: Google Maps search uses place name + area + region instead of address

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,7 +9,8 @@ export function getRegionName(region) {
 }
 
 export function buildMapsLink(place) {
-    const parts = [place.name, place.area, place.address].filter(Boolean)
+    const regionName = getRegionName(place.region)
+    const parts = [place.name, place.area, regionName].filter(Boolean)
     const query = parts.join(' ')
     return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}&hl=zh-TW`
 }
